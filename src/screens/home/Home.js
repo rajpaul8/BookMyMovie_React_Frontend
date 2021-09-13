265db9cd5488803c0e1fd269231e4b8cc195d3f6
rajpaul8 <rajpaul83@gmail.com>
2021-09-13 13:02:45 +0530

    Project Statement For Header Completed

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,6 +1,23 @@
+import React, { useEffect, useState } from "react";
+import {
+  GridListTile,
+  GridList,
+  GridListTileBar,
+  Card,
+  withStyles,
+  CardContent,
+  Select,
+  MenuItem,
+  Checkbox,
+  InputLabel,
+  FormControl,
+  TextField,
+  Button,
+} from "@material-ui/core";
+
 import "./Home.css";
 import Header from "../../common/header/Header";
-import React from "react";
+import FormInput from "../../common/formInput/FormInput";
 
 export default function Home (props){
 
